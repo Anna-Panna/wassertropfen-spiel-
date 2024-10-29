a9aff06b5ffccae9cc32be19d48048ee50caf079
Anna-Panna <anna.m.neumeier@icloud.com>
2024-10-29 08:55:39 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,7 @@ let eimer: game.LedSprite = null
 let wasser = game.createSprite(2, 0)
 eimer = game.createSprite(2, 4)
 game.setLife(3)
+let leben = 3
 game.setScore(0)
 basic.forever(function () {
     if (wasser.get(LedSpriteProperty.Y) < 4) {
@@ -17,8 +18,17 @@ basic.forever(function () {
             basic.setLedColors(0x00ff00, 0x00ff00, 0x00ff00)
             game.addScore(1)
         } else {
-            basic.setLedColors(0xff0080, 0xff0080, 0xff0080)
             game.removeLife(1)
+            leben = leben - 1
+            if (leben == 3) {
+                basic.setLedColors(0x00ff00, 0x00ff00, 0x00ff00)
+            } else if (leben == 2) {
+                basic.setLedColors(0x00ff00, 0x00ff00, 0x000000)
+            } else if (leben == 1) {
+                basic.setLedColors(0x00ff00, 0x000000, 0x000000)
+            } else if (leben == 0) {
+                basic.setLedColors(0x000000, 0x000000, 0x000000)
+            }
         }
         wasser.set(LedSpriteProperty.X, randint(0, 4))
         wasser.set(LedSpriteProperty.Y, 0)
